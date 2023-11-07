914d1f29855688336b20eb336ea39e5f59595046
Татьяна Мартынова <tanyamartynova30@gmail.com>
2023-11-07 14:30:49 +0300

    добавлена функция max

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-
+int max( int i1, int i2);
 int main (){
-    std::cout<< "Hellow World!"<< std::endl;
+    std::cout<< " Наибольшее число: "<<  max (1, 2) << std::endl;
 
 return 0;
+
+}
+
+
+
+
+int max( int i1, int i2){
+    if (i1>i2) return i1;
+
+    return i2;
 
 }
