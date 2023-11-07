e4b7efad0ce886c94bdc630d7c8bbdfcc26f05d1
Миша Спиридонов <jacknap1er@mail.ru>
2023-11-07 14:25:01 +0300

    max

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
-int main(){
-std:: cout << "Hello world!" << std :: endl; 
+int max (int i1, int i2);
 
+
+int main(){
+    std:: cout << "Наибольшее число: " << max(1,2) << std :: endl; 
+    
+
+
+    return 0;
 }
 
 
 int max (int i1, int i2){
-    
+    if (i1 > i2) return i1;
+
+    return i2;
 }
