b1ba87f090c610ead749b7ff89fb03f318609a3a
Alexander Borisov <albor68@gmail.com>
2023-11-07 15:04:46 +0300

    Исходный текст разделен на модули

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
+#include "functions.h"
+
 #include <iostream>
 
-int max(int i1, int i2);
 
 int main () {
 
      std::cout << "Наибольшее число: " << max(1, 2) << std::endl;
      
      return 0;
-
-}
-
-
-int max (int i1, int i2) {
-
-     if (i1 > i2) return i1;
-
-     return i2;
 
 }
