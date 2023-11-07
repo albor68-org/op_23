82ad976c3fe50ad6cd3426c0f7d6dff7995d3111
Минюков Илья <ilha.minyukov@yandex.ru>
2023-11-07 14:31:55 +0300

    максимум

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int max (int i1, int i2);
+
+
+int main() {
+
+    std::cout << "наибольшее число: " <<max(1, 2) << std :: endl;
+    
+    return 0;
+
+}
+
+
+
+int max (int i1, int i2){
+
+if (i1 > i2) return i1;
+
+return i2;
+
+}
