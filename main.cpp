4262e488cc4acb15de8c735e1e29753e33f15da5
Михаил Будылин <bmichailabc@gmail.com>
2023-11-07 14:30:21 +0300

    Создана функция max

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+int max(int i1, int i2);     // Прототип функции
+
 int main() {
-    std::cout << "HELLO WORLD" << std::endl;
+
+    std::cout << "Наибольшее число:" << std::endl;
+    std::cout << max(10, 11) << std::endl;
+
     return 0;
+}
+
+int max(int i1, int i2) {     // Функция поиска максимума из двух чисел
+
+    if (i1 > i2) return i1;
+
+    return i2;
 }
