9b9975d38a0de9a2be739057ee1070a2f6a6294b
Leonid Bakaev <bakaev.leonid62@yandex.ru>
2023-11-07 15:04:38 +0300

    Исходный текст разделён на модули

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include "functions.h"
+
 #include <iostream>
 
 int main () {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Наибольшее число: " << max(1,2) << std::endl;
 
     return 0;
 }
