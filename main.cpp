c4f3bd56452627874d17b44f3b4eee2a1ffd37d9
Василий Зенкин <zvs2004.31@gmail.com>
2023-11-07 14:30:07 +0300

    Добавлена функция поиска максимума

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+int max(int i1, int i2); //Прототип функции
+
 int main() 
 {
-    std::cout << "Hello world!" << std::endl;
-
+   // std::cin >> i1 >> std::endl;
+    std::cout << "Наибольшее число: " << max(10,2) << std::endl;
+    
     return 0;
+}
+
+int max(int i1, int i2)
+{
+    if (i1 > i2) return i1;
+    return i2;
+
 }
