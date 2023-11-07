6672db88e4c629606a0a9bc9ccc23b347666a783
Михаил Будылин <bmichailabc@gmail.com>
2023-11-07 15:05:24 +0300

    Исходный код разделили на модули

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
+#include "functions.h"
 #include <iostream>
-
-int max(int i1, int i2);     // Прототип функции
 
 int main() {
 
@@ -8,11 +7,4 @@ int main() {
     std::cout << max(10, 11) << std::endl;
 
     return 0;
-}
-
-int max(int i1, int i2) {     // Функция поиска максимума из двух чисел
-
-    if (i1 > i2) return i1;
-
-    return i2;
 }
