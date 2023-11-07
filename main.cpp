dd6b54500aa0657353a9d79476bd1f5290042b22
Савин Никита <svnnikitos@gmail.com>
2023-11-07 15:03:28 +0300

    проект был разделен на 3 модуля

--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
-#include <iostream> 
+#include "functions.h" 
 
-int max (int i1, int i2);
+#include <iostream> 
 
 int main () {
  
     std::cout << "наибольшее число: " << max(1, 2) << std::endl;
     return 0;
-
-}
-
-
-int max (int i1, int i2) {
-
-    if (i1 > i2) return i1;
-    return i2;
 
 }
