9add86836316bbfa4048225bae6be053d16c40f5
Миша Спиридонов <jacknap1er@mail.ru>
2023-11-07 15:04:32 +0300

    программа разбита на несколько файлов :)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
+#include "functions.h"
 
-int max (int i1, int i2);
+
 
 
 int main(){
-    std:: cout << "Наибольшее число: " << max(1,2) << std :: endl; 
+    std:: cout << "Наибольшее число: " << max(1,5) << std :: endl; 
     
 
 
     return 0;
-}
-
-
-int max (int i1, int i2){
-    if (i1 > i2) return i1;
-
-    return i2;
 }
