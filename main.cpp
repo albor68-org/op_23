df595cde7c13775e899a7c2c039a88585ca721d4
Пантилеев Данила <neeta7love@gmail.com>
2023-11-07 15:05:50 +0300

    Split the source text into modules

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "functions.h"
 
 int main () {
     std::cout << "Наибольшее число: " << std::max(1, 2) << std::endl;   
@@ -7,10 +7,3 @@ int main () {
     return 0;    
 }
 
-int max (int i1, int i2) {
-
-    if (i1 > i2) return i1;
-
-    return i2;
-    
-}
