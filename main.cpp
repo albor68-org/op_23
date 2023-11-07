d4381ee266e70533ebb49b91097e799672133a83
Georgiy Starostin <georgst299@mail.ru>
2023-11-07 15:05:40 +0300

    Программа разбита на несколько файлов

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
+double max (double i1 , double i2);
 
 int main (){
 
-std::cout << "Привет, мир" << std::endl;
+
+std::cout << "Наибольшее число: " << max(5.8 , 3)<< std::endl;
 
 
     return 0;
 
 
+}
+
+
+double max (double i1 , double i2){
+
+    if (i1 > i2) return i1;
+
+    return i2;
 }
