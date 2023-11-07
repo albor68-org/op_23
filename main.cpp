ef657ae5edd63f6f03f0004f99e14183d074e31b
Савин Никита <svnnikitos@gmail.com>
2023-11-07 14:30:07 +0300

    создана функция max

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream> 
 
+int max (int i1, int i2);
+
 int main () {
  
-    std::cout << "hello, world!" << std::endl;
+    std::cout << "наибольшее число: " << max(1, 2) << std::endl;
     return 0;
+
+}
+
+
+int max (int i1, int i2) {
+
+    if (i1 > i2) return i1;
+    return i2;
 
 }
