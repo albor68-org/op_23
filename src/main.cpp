a815d824430c8891c5f28840962baba906f86f5d
insanewave <alxbelt@mail.ru>
2023-11-17 11:19:32 +0300

    edited directory

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include "function.h" // подключаем свою библиотеку
+
+int main(){
+using namespace std;
+
+int a = 20;
+int b = 21;
+
+
+cout<<"max is: "<<max(a,b)<<endl;
+
+    return 0;
+}
+
