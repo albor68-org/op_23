c8cdb6d3ea2221856a5aef4edb42f4923a50e39b
Артём Анашкин <artem.anashkin.2017@mail.ru>
2023-11-17 11:06:45 +0300

    Создание сценария проекта

--- main.ccp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-
-    std::cout << "Hello world" << endl;
-    
-    return 0;
-
-}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "functions.h"
+
+
+int main() {
+
+    std::cout << "Наибольшее число: " << max(1,4) << std::endl;
+    
+    return 0;
+
+}
