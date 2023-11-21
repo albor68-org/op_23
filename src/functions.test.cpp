e938eaa7af4bc7c52c894c7f9e77555d1efd27dc
Михаил Будылин <bmichailabc@gmail.com>
2023-11-21 15:10:35 +0300

    Добавлены тесты

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -4,5 +4,10 @@
 TEST_CASE(){
 
     CHECK(max(1, 2) == 2);
+    CHECK(max(-1,-2) == -1);
+    CHECK(max(100, 200) == 200);
+    CHECK(max(-1, 2) == 2);
+    CHECK(max(0,-2) == 0);
+    CHECK(max(0, 1) == 1);
 
 }
