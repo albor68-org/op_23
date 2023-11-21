a1b5ba5f379c6e40f9da9dad07c8cfabf46cd45e
Василий Зенкин <zvs2004.31@gmail.com>
2023-11-21 15:01:56 +0300

    Создан модульный тест

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,7 @@
 
 int main() 
 {
-   // std::cin >> i1 >> std::endl;
-    std::cout << "Наибольшее число: " << max(10,2) << std::endl;
+    std::cout << "Наибольшее число: " << max(16,6) << std::endl;
     
     return 0;
 }
