d840e99334984349130aa7c0e4c3ad7fae31d1d3
Кондрахин Илья <bapcyk313@mail.ru>
2023-12-01 10:41:21 +0300

    Изменен function.test

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -4,8 +4,10 @@
 
 TEST_CASE() {
 
-    CHECK(max(1, 2) == 2);
-    CHECK(max(-1, -2) == -1);
-    CHECK(max(0, 10) == 10);
-    CHECK(max(-2, 2) == 2);
+    CHECK(sum(1, 2) == 3);
+    CHECK(sum(-1, -2) == -3);
+    CHECK(sum(0, 10) == 10);
+    CHECK(area(3, 2) == 6);
+    CHECK(area(3, 0) == 0);
+    CHECK(area(1, 6) == 6);
 }
