12c3259395c67b8154fbc64a8d0da0f8382d000b
Никита Васильев <mrslademc@gmail.com>
2023-12-01 11:09:15 +0300

    Добавлена функция сложения и вычисления площади

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,4 +1,11 @@
+#include <math.h>
 double max (double i1, double i2){
     if (i1 < i2) return i2;
     return i1;
+}
+int sum (int s1, int s2){
+    return s1+s2;
+}
+int area (int a, int b){
+    return fabs(a*b);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "functions.h"
+#include <math.h>
 
 int main() {
     std::cout << "Наибольшее число:" << max(5.8, 3.1) << std::endl;
