2ee41e0ad8c39c1e79e9460ad690b02e284ac147
Михаил Гришин <bobbyrobinbo@gmail.com>
2023-12-01 10:55:17 +0300

    исправлены ошибки

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -1,9 +1,9 @@
 #include "functions.h"
 #include <catch2/catch.hpp>
 
-TEST_CASE() {
-    CHECK(max(1, 2)==2);
-    CHECK(max(-1,- 2)==-1);
-    CHECK(max(0, 10)==10);
-    CHECK(max(-2, 2)==2);
+TEST_CASE(){
+    CHECK(max(1, 2) == 2);
+    CHECK(max(-1,- 2) == -1);
+    CHECK(max(0, 10) == 10);
+    CHECK(max(-2, 2) == 2);
 }
