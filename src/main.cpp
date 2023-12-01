84efc9f6819c387ac4f065c66be05a3927571f3d
Пантилеев Данила <neeta7love@gmail.com>
2023-12-01 11:06:50 +0300

    Implemented sum() and rectArea() in main()

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,11 @@
 
 int main () {
     std::cout << "Наибольшее число: " << std::max(1, 2) << std::endl;
+
+    std::cout << "Сумма: " << sum(5, 4) << std::endl;
+
+    std::cout << "Площадь прямоугольника экранной формы: " << rectArea(30, 40) << std::endl;
+
     return 0;    
 }
 
