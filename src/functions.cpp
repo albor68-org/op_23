94dd1875e15e77ea82aa36555ef7bab0acd81083
Пантилеев Данила <neeta7love@gmail.com>
2023-12-01 10:44:02 +0300

    Added rectangle area calculation

--- src/functions.cpp
+++ src/functions.cpp
@@ -10,3 +10,13 @@ int sum (int i1, int i2) {
     int i3 = i1 + i2;
     return i3;
 }
+
+int rectArea (int height, int width) {
+    if (height >= 0 && width >= 0) {
+        int result = height * width;
+        return result;
+    }
+    else {
+        return 0;
+    }
+}
