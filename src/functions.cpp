cf0a262703c9d7757d5546256b86cdb09ba4e3cb
Рункова Катя <kateru68@gmail.com>
2023-12-01 11:19:53 +0300

    добавлены функции вычисления суммы и площади

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,3 +3,11 @@ int max(int i1, int i2){
     if (i1>i2) return i1;
     return i2;
 }
+
+int sum(int s1, int s2){
+    return s1+s2;
+}
+
+int square(int sq1, int sq2){
+    return sq1*sq2;
+}
--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -7,3 +7,11 @@ TEST_CASE() {
     CHECK(max(0, 10)==10);
     CHECK(max(-2, 2)==2);
 }
+TEST_CASE(){
+    CHECK(sum(2, 2)==4);
+    CHECK(sum(-2, 2)==0);
+}
+TEST_CASE(){
+    CHECK(square(3, 4)==12);
+    CHECK(square(10, 1)==10);
+}
