c4c026014ad74e8e8982312ffd3b73bfd5c2eb6e
Пантилеев Данила <neeta7love@gmail.com>
2023-12-01 10:35:24 +0300

    Added sum() to the unit testing and deleted it from main()

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -1,9 +1,18 @@
 #include "functions.h"
 #include <catch2/catch.hpp>
 
+//тест для максимума
 TEST_CASE() {
     CHECK(max(1, 2) == 2);
     CHECK(max(-1, -2) == -1);
     CHECK(max(0, 10) == 10);
     CHECK(max(-2, 2) == 2);
+}
+
+//тест для суммы
+TEST_CASE() {
+    CHECK(sum(5, 4) == 9);
+    CHECK(sum(-2, 2) == 0);
+    CHECK(sum(-5, -4) == -9);
+    CHECK(sum(0, 3) == 3);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 
 int main () {
     std::cout << "Наибольшее число: " << std::max(1, 2) << std::endl;
-    std::cout << "Сумма двух чисел: " << std::sum(25, 26) << std:endl;
     return 0;    
 }
 
