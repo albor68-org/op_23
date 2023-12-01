7e6eb2dfd70d1a11f9a901b20a0d6df228eeeb25
Михаил Будылин <bmichailabc@gmail.com>
2023-12-01 11:12:15 +0300

    Добавлена реализация функций в main

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,35 @@
 #include <iostream>
 
 int main() {
-    int triangle_height{100};
-    int triangle_lenght{20};
+    /*
+    int triangle_height;
+    int triangle_lenght;
 
-    int square_height{100};
-    int square_lenght{200};
-
-    int i1{10};
-    int i2{1000};
-
+    int square_height;
+    int square_lenght;
+    */
+    int i1;
+    int i2;
+    std::cout << "Запишите 2 числа, для поиска наибольшего" << std::endl;
+    std::cin >> i1;
+    std::cin >> i2;
     std::cout << "Наибольшее число:" << std::endl;
-    std::cout << max(10, 11) << std::endl;
+    std::cout << max(i1, i2) << std::endl;
+    std::cout << "Запишите 2 числа, которые нужно сложить" << std::endl;
+    std::cin >> i1;
+    std::cin >> i2;
     std::cout << "Сложение чисел:" << std::endl;
     std::cout << sum(i1,i2) << std::endl;
+    std::cout << "Запишите 2 числа, высоту и длину прямоугольного треугольника" << std::endl;
+    std::cin >> i1;
+    std::cin >> i2;
     std::cout << "Площадь треугольника:" << std::endl;
-    std::cout << triangle_square(triangle_height, triangle_lenght) << std::endl;
+    std::cout << triangle_square(i1, i2) << std::endl;
+    std::cout << "Запишите 2 числа, высоту и ширину прямоугольник" << std::endl;
+    std::cin >> i1;
+    std::cin >> i2;
     std::cout << "Площадь прямоугольника:" << std::endl;
-    std::cout << square(square_height, square_lenght) << std::endl;
+    std::cout << square(i1, i2) << std::endl;
 
     return 0;
 }
