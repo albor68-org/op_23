26800d8ef542d0f5be47e60937ee95317f478a1f
Михаил Гришин <bobbyrobinbo@gmail.com>
2023-12-01 11:24:49 +0300

    Сделана половина 6 лабораторной

---
+++ src/functions.h
@@ -0,0 +1,3 @@
+int max (int i1 , int i2);
+int summ (int i3 , int i4);
+int sq (int i5 , int i6);
--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -2,8 +2,12 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE(){
-    CHECK(std::max(1,2) == 2);
-    CHECK(std::max(-1,-2) == -1);
-    CHECK(std::max(0,10) == 10);
-    CHECK(std::max(-2,2) == 2);
+    CHECK(max(1,2) == 2);
+    CHECK(max(-1,-2) == -1);
+    CHECK(max(0,10) == 10);
+    CHECK(max(-2,2) == 2);
+}
+TEST_CASE(){
+    CHECK(summ(-2,2) == 0);
+    CHECK(summ(1,2) == 3);
 }
