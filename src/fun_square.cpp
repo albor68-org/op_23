750ac4bb81dd313ac659880edb295bb6f5bf9f6d
Georg <georgst299@mail.ru>
2023-12-05 00:13:21 +0300

    Забыл поставить стартовый коммит :(

---
+++ src/fun_square.cpp
@@ -0,0 +1,9 @@
+double square(double R){
+    bool i;    //Переменная используется для тестирования
+
+    if (R <= 0){ i = 0;}
+
+    else { i = 1; }
+
+    return R * R * 3.14 * i ;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,23 @@
 #include "functions.h"
 
 int main (){
+   double R = 3;                                     //Задаём значение радиуса
 
-std::cout << "Наибольшее число: " << max(5.8 , 3.1)<< std::endl;
+   std::cout << "The largest number: "
+             << max(5.8 , 3.1)<< std::endl;         //Вывод максимального числа
+   std::cout << "The sum of two numbers: "
+             << sum(2.2 , 3.5) << std::endl;        //Вывод суммы чисел
 
+   if ( square(R) == 0){
+       std::cout << "The area is missing"           //Если площадь окр. будет равна нулю или
+                 << std::endl;                      //будет отрицательной, то выводится сообщение
+   }
+   else {
+      std::cout << "The area of the circle: "
+                << square(R) << std::endl;          //Если площадь больше нуля,
+   }                                                //то выводим результат
 
     return 0;
-
-
 }
 
 
