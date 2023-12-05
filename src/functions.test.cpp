831d03dbe590487e9229d1151b299ab76f5b9f2b
Савин Никита <svnnikitos@gmail.com>
2023-12-05 13:53:31 +0300

    исправлены ошибки

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -6,11 +6,15 @@ TEST_CASE() {
     CHECK(max(-1, -2) == -1);
     CHECK(max(0, 10) == 0);
     CHECK(max(-2, 2) == 2);
-    
+}
+
+TEST_CASE() {
     CHECK(sum(2, 2) == 2);
     CHECK(sum(2.2, 2) == 5.5);
     CHECK(sum(0, 2) == 2);
+}
 
+TEST_CASE() {
     CHECK(square(-1, 2) == 2);
     CHECK(square(4, 2) == 8);
     CHECK(square(3, -3) == 9);
