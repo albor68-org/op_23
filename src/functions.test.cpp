1cfc5699f130423bc619b3f4bca8effc3e6abfd2
Кондрахин Илья <bapcyk313@mail.ru>
2023-12-05 14:48:16 +0300

    Исправлены ошибки

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -2,15 +2,20 @@
 #include <catch2/catch.hpp>
 #include <math.h>
 
-TEST_CASE() {
-
-    CHECK(area(3, 2) == 6);
-    CHECK(area(3, 0) == 0);
-    CHECK(area(1, 6) == 6);
-}
-TEST_CASE() {
+TEST_CASE("Сумма") {
 
     CHECK(sum(1, 2) == 3);
     CHECK(sum(-1, -2) == -3);
     CHECK(sum(0, 10) == 10);
+}
+TEST_CASE("Площадь") {
+
+    CHECK(area(3, 2) == 6);
+    CHECK(area(1, 6) == 6);
+}
+TEST_CASE("Площадь (Ошибка в данных)") {
+
+    CHECK(area(0, 3) == 0);
+    CHECK(area(-2, 7) == 0);
+    CHECK(area(-1, -3) == 0);
 }
