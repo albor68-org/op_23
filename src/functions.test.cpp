6834ffa4e3551843c198584a7587de6b4acd2adf
Иван Савушкин <ridorust10fps@gmail.com>
2023-12-08 09:36:31 +0300

    обраюотка исключений в функции main

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -7,13 +7,8 @@ TEST_CASE(){
     CHECK(max(-1, -2) == -1);
     CHECK(max(0, 10) == 10);
     CHECK(max(-2, 2) == 2);
-    TEST_CASE(){
     CHECK(summ(-2, 2) == 0);
     CHECK(summ(-20, 2) == -18);
-    }
-    TEST_CASE(){
     CHECK(area(6, 2) == 12);
-
-    }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 
 int main () try  {
 
- std::cout << "Наибольшое число " << max (1, 2) << std::endl; 
+std::cout << "Наибольшое число " << max (1, 2) << std::endl; 
 
  std::cout << "сумма = " << summ (2, 5) << std::endl; 
 
- std::cout << "s = " << area (2, 5) << std::endl;
+ std::cout << "s = " << area (4, -2) << std::endl;
 
 int s = area(4, -2);
 
@@ -18,8 +18,14 @@ int s = area(4, -2);
 
 }
 catch (area_error&){
+  std::cerr << "area: нарушены предусловия" 
+  << " аргумены функции должны быть больше 0"
+  << std::endl;
      return 1;
 }
 catch(...){
+  std::cerr << "нет слов, одни эмоции" 
+  << " что-то пошло не так"
+  << std::endl;
     return 2;
 }
