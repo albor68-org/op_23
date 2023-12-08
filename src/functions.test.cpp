b0554f2cfa7f9c2214558cf04d787ba7f4e4266c
Иван Савушкин <ridorust10fps@gmail.com>
2023-12-08 09:48:05 +0300

    добавлены доп. тесты

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -1,14 +1,28 @@
 #include "functions.h"
 #include <catch2/catch.hpp>
 
-TEST_CASE(){
+TEST_CASE("max"){
     
     CHECK(max(1, 2) == 2);
     CHECK(max(-1, -2) == -1);
     CHECK(max(0, 10) == 10);
     CHECK(max(-2, 2) == 2);
+}
+
+TEST_CASE("summ"){
     CHECK(summ(-2, 2) == 0);
     CHECK(summ(-20, 2) == -18);
-    CHECK(area(6, 2) == 12);
+}
 
+TEST_CASE("area norm"){
+    CHECK(area(6, 2) == 12);
+    CHECK(area(10, 15) == 150);
+}
+
+TEST_CASE("area ne norm"){
+    CHECK_THROWS(area(6, -2));
+    CHECK_THROWS(area(-10, -15));
+    CHECK_THROWS(area(-10, 15));
+    CHECK_THROWS(area(-10, 0));
+    CHECK_THROWS(area(0, 15));
 }
