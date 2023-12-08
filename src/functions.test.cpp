3356984355d385cb39ccba3e7d90f9826ea9d616
Савин Никита <svnnikitos@gmail.com>
2023-12-08 09:49:12 +0300

    созданы тесты

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -1,21 +1,27 @@
 #include "functions.h"
 #include <catch2/catch.hpp>
 
-TEST_CASE("МАКСИМУМ") {
-    CHECK(max(1, 2) == 2);
-    CHECK(max(-1, -2) == -1);
-    CHECK(max(0, 10) == 0);
-    CHECK(max(-2, 2) == 2);
-}
+//TEST_CASE("МАКСИМУМ") {
+  //  CHECK(max(1, 2) == 2);
+    //CHECK(max(-1, -2) == -1);
+   // CHECK(max(0, 10) == 0);
+   // CHECK(max(-2, 2) == 2);
+//}
 
-TEST_CASE("СУММА") {
-    CHECK(sum(2, 2) == 2);
-    CHECK(sum(2, 2) == 5);
-    CHECK(sum(0, 2) == 2);
-}
+//TEST_CASE("СУММА") {
+//CHECK(sum(2, 2) == 2);
+//CHECK(sum(2, 2) == 5);
+//CHECK(sum(0, 2) == 2);
+//}
 
-TEST_CASE("ПЛОЩАДЬ") {
+TEST_CASE("area true") {
     CHECK(area(1, 2) == 2);
-    CHECK(area(4, 2) == 7);
+    CHECK(area(4, 2) == 8);
     CHECK(area(3, 3) == 9);
+}
+
+TEST_CASE("area false") {
+    CHECK_THROWS(area(1, -2));
+    CHECK_THROWS(area(4, -2));
+    CHECK_THROWS(area(-3, 0));
 }
