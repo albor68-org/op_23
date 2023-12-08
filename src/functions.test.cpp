ff326e4b8a1af92e68550e2fc5278dc1216f9638
Иван Савушкин <ridorust10fps@gmail.com>
2023-12-08 09:13:29 +0300

    добален перехват исключений

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -7,10 +7,13 @@ TEST_CASE(){
     CHECK(max(-1, -2) == -1);
     CHECK(max(0, 10) == 10);
     CHECK(max(-2, 2) == 2);
+    TEST_CASE(){
     CHECK(summ(-2, 2) == 0);
     CHECK(summ(-20, 2) == -18);
-    CHECK(sq(6, 2) == 12);
+    }
+    TEST_CASE(){
+    CHECK(area(6, 2) == 12);
 
-
+    }
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,22 @@
 
 
 
-int main () {
+int main () try  {
 
-  std::cout << "Наибольшое число " << max (1, 2) << std::endl; 
+ std::cout << "Наибольшое число " << max (1, 2) << std::endl; 
 
-  std::cout << "сумма = " << summ (2, 5) << std::endl; 
+ std::cout << "сумма = " << summ (2, 5) << std::endl; 
 
-   std::cout << "s = " << sq (2, 5) << std::endl;
+ std::cout << "s = " << area (2, 5) << std::endl;
+
+int s = area(4, -2);
 
   return 0;
 
+}
+catch (area_error&){
+     return 1;
+}
+catch(...){
+    return 2;
 }
