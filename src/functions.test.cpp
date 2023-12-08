8953c7a23a1412b933c63925bea3fa31be25fc52
Пантилеев Данила <neeta7love@gmail.com>
2023-12-08 09:46:19 +0300

    Better readability in functions.test.cpp

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -3,23 +3,29 @@
 
 //тест для максимума
 TEST_CASE("Максимум") {
+
     CHECK(max(1, 2) == 2);
     CHECK(max(-1, -2) == -1);
     CHECK(max(0, 10) == 10);
     CHECK(max(-2, 2) == 2);
+
 }
 
 //тест для суммы
 TEST_CASE("Сумма") {
+
     CHECK(sum(5, 4) == 9);
     CHECK(sum(-2, 2) == 0);
     CHECK(sum(-5, -4) == -9);
     CHECK(sum(0, 3) == 3);
+
 }
 
 //тест для умножения
 TEST_CASE("Площадь") {
+
     CHECK(rectArea(5, 4) == 20);
     CHECK(rectArea(1, 3) == 3);
     CHECK(rectArea(20, 40) == 800);
+    
 }
