da0e1715b2d8e7f6318bfee7764e28c51610878a
Пантилеев Данила <neeta7love@gmail.com>
2023-12-08 09:09:20 +0300

    Implemented exceptions in main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include "functions.h"
 
-int main () {
+
+int main () try {
+
     std::cout << "Наибольшее число: " << max(1, 2) << std::endl;
 
     std::cout << "Сумма: " << sum(5, 4) << std::endl;
 
-    std::cout << "Площадь прямоугольника экранной формы: " << rectArea(30, 40) << std::endl;
+    std::cout << "Площадь прямоугольника экранной формы: " << rectArea(4, 5) << std::endl;
+    
+    return 0;  
 
-    return 0;    
+}
+
+catch (Area_error&) {
+
+    std::cout << "[Ошибка в задании длины и ширины прямоугольника экранной формы]" << std::endl;
+
+    return 1;
+
+}
+
+catch (...) {
+
+    std::cout << "[Ошибка]" << std::endl;
+
+    return 2;
+
 }
 
