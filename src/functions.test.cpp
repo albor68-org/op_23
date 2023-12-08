33ff4e1cf28a395a4ae52da36c3cc927e3e9816f
Морозов Глеб <morozov.g00@mail.ru>
2023-12-08 09:49:19 +0300

    Лабораторная работа 7

--- src/functions.test.cpp
+++ src/functions.test.cpp
@@ -1,7 +1,7 @@
 #include "functions.h"
 #include <catch2/catch.hpp>
 
-    TEST_CASE() {
+    TEST_CASE("Максимум из двух чисел") {
 
     CHECK(max(1, 2) == 2);
 
@@ -12,10 +12,21 @@
     CHECK(max(-2, 2) == 2);
     }
 
-    TEST_CASE() {
+    TEST_CASE("Сумма") {
     CHECK(CYMM(3, 3)== 6);
+    CHECK(CYMM(6, 3)== 9);
+    CHECK(CYMM(3, 9)== 12);
     }
 
-    TEST_CASE() {  
+    TEST_CASE("area norm") {  
     CHECK(area(2,5)== 10);
+    CHECK(area(40,6)== 240);
+    
+    }
+
+    TEST_CASE("area abnormal") {  
+    CHECK_THROWS(area(-2,5));
+    CHECK_THROWS(area(23,-34));
+    CHECK_THROWS(area(-3,-232));
+    CHECK_THROWS(area(0,6));
     }
