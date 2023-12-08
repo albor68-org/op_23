436a73f170b5811c0a8a851bc32f49982b292669
Савин Никита <svnnikitos@gmail.com>
2023-12-08 09:32:47 +0300

    изменены сообщения об ошибках

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,12 @@ int main () try {
     return 0;
 }
 catch (Area_error&) {
+    std::cerr << "area: нарушены предусловия," 
+    << " аргументы функции должны быть больше нуля" 
+    << std::endl;
     return 1;
 }
 catch (...) {
+    std::cerr << "что-то пошло не так... попробуй еще раз!" << std::endl;
     return 2;
 }
