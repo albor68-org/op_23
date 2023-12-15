0d4f8d6d68dc264313aa7339fd48915dc8e3f7ea
Александр Бобров <sasha12bobrov@mail.ru>
2023-12-15 11:27:55 +0300

    минимальны максимальные значения

--- src/function.cpp
+++ src/function.cpp
@@ -1,4 +1,6 @@
 #include "function.h"
+#include <vector>
+
 int max(int i1, int i2) {
     if(i1>i2) return i1;
     return i2; 
@@ -23,3 +25,29 @@ int gcd (int a, int b)
 
     }
 }
+ int contrast(std::vector<int> v)
+ {
+    std::vector<int> checkV = v;
+
+    int tempMax = checkV[0];
+    int maxV = 0;
+
+    int tempMin = checkV[0];
+    int minV = 0 ;
+
+    for (int elMax : checkV){
+        if (elMax >= tempMax){
+            tempMax = elMax;
+            maxV = elMax;
+        }
+
+    }
+    for (int elMin : checkV){
+        if(elMin<= tempMin){
+            tempMin = elMin;
+            minV = elMin;
+        }
+    }
+    return (maxV - minV);
+ 
+ }
