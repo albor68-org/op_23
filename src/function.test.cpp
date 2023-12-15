9bb802feac7ac6990eaf8c6a2752f62be67fb1c0
Александр Бобров <sasha12bobrov@mail.ru>
2023-12-15 11:32:37 +0300

    тесты мин\макс

--- src/function.test.cpp
+++ src/function.test.cpp
@@ -21,3 +21,9 @@ TEST_CASE("area abnormal") {
 CHECK_THROWS (square(-3,2));
 CHECK_THROWS (square(40,-4));
 }
+TEST_CASE("contrast test")
+{
+    CHECK(contrast ({5,2,3,6,8,29})== 27);
+    CHECK(contrast ({4,20,7,8})== 16);
+    CHECK(contrast ({26,40,3,14})== 37);
+}
