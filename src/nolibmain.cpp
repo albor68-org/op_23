5efa219b191b05404031aade9eae365cffec1600
Alexander Beltiukov <alxbelt@mail.ru>
2023-12-24 22:18:26 +0300

    добавлен комментарий к тестовому коду

--- src/nolibmain.cpp
+++ src/nolibmain.cpp
@@ -1,5 +1,5 @@
 
-#include <iostream>
+#include <iostream> // тестовый файл для проверки правильной работы кода, сваленного в одну кучу
 using namespace std;
 int max(int a, int b){
     if(a<b){return(b);}else{return(a);}
